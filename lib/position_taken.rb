c451e8118a28b543c55809bc422a6a603f3c707e
Catherine Bronzo <cbronzo@mac.com>
2020-11-28 03:22:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken? (board, index)
-  if board[index] == " "
+  if board[index] == " " || "" || nil 
     puts false
-  elsif board[index] == ""
+  else board[index] == "X" || "O"
+    puts true
   end
 end
